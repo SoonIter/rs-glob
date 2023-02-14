12ecdc35c58781ee8c4c871e5bd45df2754d2cfc
github-action-benchmark <github@users.noreply.github.com>
2023-02-14 19:16:33 +0000

    add Rust Benchmark (cargo) benchmark result for 63b394632717b950bfbe992bcbd99222b4723832

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1676402193074,
+  "repoUrl": "https://github.com/SoonIter/rs-glob",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sooniter@gmail.com",
+            "name": "SoonIter",
+            "username": "SoonIter"
+          },
+          "committer": {
+            "email": "sooniter@gmail.com",
+            "name": "SoonIter",
+            "username": "SoonIter"
+          },
+          "distinct": true,
+          "id": "63b394632717b950bfbe992bcbd99222b4723832",
+          "message": "fix(CI): criterion_rs  bench",
+          "timestamp": "2023-02-15T03:01:55+08:00",
+          "tree_id": "dc3328a4ca77ee16dd803bcfdb948e921fef5def",
+          "url": "https://github.com/SoonIter/rs-glob/commit/63b394632717b950bfbe992bcbd99222b4723832"
+        },
+        "date": 1676402192027,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "glob_benchmark/native_glob",
+            "value": 57770,
+            "range": "± 2093",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "glob_benchmark/wax_glob",
+            "value": 323989,
+            "range": "± 808",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
